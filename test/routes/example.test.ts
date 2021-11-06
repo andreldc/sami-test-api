996c810522dfe5e714ac012b4e0a20f5a6a4facf
andreldc <andreldc@hotmail.com>
2021-11-06 13:54:59 -0300

    chore: add jest and remove tap

--- test/routes/example.test.ts
+++ test/routes/example.test.ts
@@ -1,12 +1,11 @@
-import { test } from 'tap'
 import { build } from '../helper'
 
-test('example is loaded', async (t) => {
-  const app = await build(t)
+test('example is loaded', async () => {
+  const app = await build()
 
   const res = await app.inject({
     url: '/example'
   })
 
-  t.equal(res.payload, 'this is an example')
+  expect(res.payload).toEqual('this is an example')
 })
