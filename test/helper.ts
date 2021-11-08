6db7073b00f300454cbc9a10778921fd8289e5a1
andreldc <andreldc@hotmail.com>
2021-11-08 10:13:48 -0300

    chore: set test database

--- test/helper.ts
+++ test/helper.ts
@@ -1,18 +1,38 @@
-// helper.ts
-
 import Fastify from 'fastify'
 import fp from 'fastify-plugin'
 import App from '../src/app'
 
-export function build () {
+async function config (): Promise<any> {
+  return {
+    typeormConfig: {
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '4231',
+      database: 'sami-test',
+      synchronize: true,
+      logging: false,
+      dropSchema: true,
+      entities: ['dist/src/entities/*.js']
+    }
+  }
+}
+
+function build () {
   const app = Fastify()
 
   beforeAll(async () => {
-    void app.register(fp(App))
+    void app.register(fp(App), await config())
     await app.ready()
   })
 
   afterAll(() => app.close())
 
   return app
+}
+
+export {
+  config,
+  build
 }
