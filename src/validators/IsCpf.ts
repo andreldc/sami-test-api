0aa94f21f891d4fa4461c1ed9bf69ff37cdd1c6c
andreldc <andreldc@hotmail.com>
2021-11-08 11:22:54 -0300

    refactor: fix isCpf validator name casing

--- src/validators/IsCpf.ts
+++ src/validators/IsCpf.ts
@@ -4,7 +4,7 @@ import { cpf } from 'cpf-cnpj-validator'
 export function IsCpf (validationOptions?: ValidationOptions) {
   return function (object: Object, propertyName: string) {
     registerDecorator({
-      name: 'IsCpf',
+      name: 'isCpf',
       target: object.constructor,
       propertyName: propertyName,
 
