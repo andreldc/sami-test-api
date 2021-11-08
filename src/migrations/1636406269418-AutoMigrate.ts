75c46a1c2f1083186d8e7b39951e642b769118fd
andreldc <andreldc@hotmail.com>
2021-11-08 18:22:11 -0300

    feat: add initial migration

---
+++ src/migrations/1636406269418-AutoMigrate.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class AutoMigrate1636406269418 implements MigrationInterface {
+  name = 'AutoMigrate1636406269418'
+
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE TABLE `beneficiary` (`created_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updated_at` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), `deleted_at` datetime(6) NULL, `id` int NOT NULL AUTO_INCREMENT, `name` varchar(200) NOT NULL, `cpf` varchar(11) NOT NULL, `rg` varchar(20) NOT NULL, `birth_date` date NOT NULL, `plan` enum (\'basic\', \'standard\', \'premium\') NOT NULL DEFAULT \'basic\', `number_of_dependents` int NULL, UNIQUE INDEX `IDX_3f82a1edaed25a9e227ac07ed8` (`cpf`), PRIMARY KEY (`id`)) ENGINE=InnoDB')
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('DROP INDEX `IDX_3f82a1edaed25a9e227ac07ed8` ON `beneficiary`')
+    await queryRunner.query('DROP TABLE `beneficiary`')
+  }
+}
